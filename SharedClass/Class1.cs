e9ddb5b688bc67d65be9582f49f89e8c6ff9c051
Tom Ordille <thordill@microsoft.com>
2024-06-18 08:43:06 -0400

    Add project files.

---
+++ SharedClass/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedClass
+{
+    public class Class1
+    {
+        public string GetClassName()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
---
+++ WebMVCMultiple/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMVCMultiple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
